f2cb0c11cf93c20b0331cd453ec6c3561443917d
Replit user <>
2023-09-15 07:31:04 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -98,7 +98,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
     if ((tree->lower_than(nodo->pair->key,key)==1){
       nodo=nodo->right;
     }
-    if ((tree->lower_than(key,nodo->pair->key)==1){
+    if ((tree->lower_than(nodo->pair->key,key)<0){
       nodo=nodo->left;
     }
   }
