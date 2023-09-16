ef3ed5ed2f231dc81fdcd79195ce39d804db04fa
Replit user <>
2023-09-16 08:07:11 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -243,16 +243,14 @@ Pair* upperBound(TreeMap* tree, void* key) {
   TreeNode *nodo=tree->root;
   
   while(nodo!=NULL){
-    //if(nodo==NULL) return NULL;
-    
-    //else{  
-        if(tree->lower_than(nodo->pair->key,key)==0){
+  
+        if(tree->lower_than(nodo->pair->key,key)<0){
           Nodo_ub = nodo->pair;
           nodo = nodo->left;
         }else{
           nodo=nodo->right;
         }
-    //}
+
   }
   return Nodo_ub;
 }
