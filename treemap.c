de08d666f5a9bacc37581b9e7b84dd0f21491c3c
Replit user <>
2023-09-16 08:01:37 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -239,7 +239,7 @@ Para implementarla puede realizar una búsqueda normal y usar un puntero a nodo 
 */
 
 Pair* upperBound(TreeMap* tree, void* key) {
-  Pair *Nodo_ub;
+  Pair *Nodo_ub=NULL;
   
   while(1){
     if(tree->root==NULL) break;
